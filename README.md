9d37bee7ab13b9d77d38322cbbccc0df9e444991
Amarnath Karthi <97amarnathk@gmail.com>
2018-10-19 02:09:56 +0530

    Create package skeleton and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# nx-control
+# nxcontrol
 A python package that provides methods for graph controllability
+
+## Development
+
+1. `git clone https://github.com/chahak13/nxcontrol.git`
+2. `cd nxcontrol`
+3. `pip install -e .` 
+
+This creates a symlink, so all changes to package are reflected across the system.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nxcontrol',
+      version='0.2',
+      description='A python package that provides methods for graph controllability',
+      url='http://github.com/chahak13/nxcontrol',
+      author='Chahak Mehta',
+      author_email='chahakmehta013@gmail.com',
+      license='MIT',
+      packages=['nxcontrol'],
+      zip_safe=False)
