34b4b23c47f38a009219d517c2a0ed934f92ce3d
chahak13 <chahakmehta013@gmail.com>
2018-11-25 16:56:27 +0530

    Added methods for maximum matching driver nodes, all driver nodes and minimum dominating set nodes

--- nxcontrol/algorithms.py
+++ nxcontrol/algorithms.py
@@ -1,4 +1,5 @@
-import networkx
+import networkx as nx
+from hopkroftKarp import HopcroftKarp
 
 def bipartite_unroll(G):
     B = nx.DiGraph()
@@ -19,6 +20,32 @@ def max_matching_driver_nodes(G):
     driver_nodes = set(-x for x in matching.keys() if matching[x] is None and x<0)
     return driver_nodes
 
-def hopcroft_karp_matching(G, left_nodes):
-    INFINITY = -1
-    
+def maximum_matching_all_driver_nodes(G):
+    '''
+    Returns the set of driver nodes that are identified by a maximal matching using the Hopcroft-Karp Algorithm.
+    '''
+    # Unroll the given directed graph into a bipartite graph
+    B = bipartite_unroll(G)
+    left_nodes = set(n for n,d in B.nodes(data=True) if d['bipartite']==0)
+    hp = HopcroftKarp(B, left_nodes)
+
+    driverNodes, all_driver_nodes = hp.match_all()
+    return driverNodes, all_driver_nodes
+
+def minimum_dominating_set(G, start_with=None):
+    all_nodes = set(G)
+    if start_with is None:
+        v = set(G).pop() # pick a node
+    else:
+        if start_with not in G:
+            raise nx.NetworkXError('node %s not in G' % start_with)
+        v = start_with
+    D = set([v])
+    ND = set(G[v])
+    other = all_nodes - ND - D
+    while other:
+        w = other.pop()
+        D.add(w)
+        ND.update([nbr for nbr in G[w] if nbr not in D])
+        other = all_nodes - ND - D
+    return D
